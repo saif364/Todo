c2739fdbca65594b6ffe15079cfd4815c138cb11
saif3062 <khalid.saifullah@azolutionse.com>
2020-11-25 12:05:36 +0600

    create Butoon

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+
+import Button from "@material-ui/core/Button";
+import { FormControl, Input, InputLabel } from "@material-ui/core";
+import Todo from "./Todo";
+
+function App() {
+  const [todos, seTodos] = useState([]);
+
+  const [input, setinput] = useState("");
+  console.log(input);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    console.log("i am saif");
+    seTodos([...todos, input]);
+    setinput("");
+  };
+
+  return (
+    <div className="App">
+      <h1>HELLO Clever programmer </h1>
+
+      <FormControl>
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+        <InputLabel>Write a To Do</InputLabel>
+        <Input
+          value={input}
+          onChange={(Event) => setinput(Event.target.value)}
+        ></Input>
+        <br></br>
+        <Button
+          disabled={!input}
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+        >
+          Add to do
+        </Button>
+
+        <ul>
+          {todos.map((todo) => (
+            <Todo text={todo}></Todo>
+            //<li>{todo}</li>
+          ))}
+        </ul>
+      </FormControl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Todo.css";
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@material-ui/core";
+
+function Todo(props) { 
+  return (
+    <List>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar></Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={props.text}
+          secondary="dummy deadline 📘 "
+        ></ListItemText>
+      </ListItem>
+    </List>
+  );
+}
+
+export default Todo;
